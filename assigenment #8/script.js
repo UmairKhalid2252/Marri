c9b5d8bff0652d12472a8a0e5cc52a28b32bfe0b
UmairKhalid2252 <123576301+UmairKhalid2252@users.noreply.github.com>
2023-02-13 13:13:29 +0500

    Add files via upload

---
+++ assigenment #8/script.js
@@ -0,0 +1,85 @@
+var name = "Umair Azmat";
+var admin = name;
+alert("Name: " + admin);
+
+
+var planetName = "Earth";
+
+
+var visitorName = "Umair Azmat";
+
+
+var favDrink = Tea;
+var favNumumber = "4";
+function myFavs() {
+      console.log(favDrink);
+      console.log(favNumumber); 
+}
+
+var person = {
+    name: "Umair",
+    age: 20
+  };
+
+var firstNumber = 3;
+var secondNumber = 3;
+var result = firstNumber + secondNumber;
+console.log(result);
+
+
+var firstNum = ("3");
+var secondNum = ("4");
+var result = firstNum / secondNum;
+console.log(result);
+
+
+var firstNumb = ("5");
+var secondNumb =("10");
+var sum = firstNum + secondNum;
+var result = sum / 5;
+console.log(result);
+
+
+var F = ("Enter the temperature in Fahrenheit");
+var C = (F - 32) * 5/9;
+console.log(C);
+
+
+var F = 50;
+var C = (F - 32) * 5/9;
+console.log( C);
+
+
+var number =prompt("8");
+var number =prompt("9");
+var sum = firstNum + secondNum;
+alert("The sum of " + firstNum + " and " + secondNum + " is " + sum);
+
+
+var numbFirst =prompt("10");
+var numbSecond =prompt("9");
+var difference = numbFirst -  numbSecond;
+console.log("The difference of " + numbFirst + " and " +  numbSecond + " is " + difference);
+
+
+var numberFirst =prompt("5");
+var numberSecond =prompt("3");
+var product = numberFirst *  numberSecond;
+console.log("The product of " + numberFirst + " and " +  numberSecond + " is " + product);
+
+
+var num_ = ("3");
+var num = ("4");
+var quotient = num_ / num;
+console.log("The quotient of " + num_ + " and " + num + " is " + quotient);
+
+
+var numb_ = ("3");
+var numb = ("4");
+var remainder = numb_ % numb;
+alert("The remainder of " + numb_ + " and " + numb + " is " + remainder);
+
+
+
+
+
