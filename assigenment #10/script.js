6e702cd97fce7e605126cfec231652a3ecbd02ad
UmairKhalid2252 <123576301+UmairKhalid2252@users.noreply.github.com>
2023-02-28 13:10:07 +0500

    Add files via upload

---
+++ assigenment #10/script.js
@@ -0,0 +1,101 @@
+// question # 2
+
+true
+false
+false
+true
+false
+false
+false
+
+//  question # 3
+
+a=1
+b=1
+c=2
+d=1
+
+//  question # 4
+
+a=4
+x=5
+
+
+//  question # 5
+
+let a = parseInt(prompt("First number?", 1));
+let b = parseInt(prompt("Second number?", 2));
+
+alert(a + b);
+
+//  question # 6
+
+let result = (a + b < 4) ? 'Below' : 'Over';
+
+//  question # 7
+
+let msg = (login == 'Employee') ? 'Hello' :
+              (login == 'Director') ? 'Greetings' :
+              (login == '') ? 'No login' :
+              '';
+
+//  question # 8
+
+2
+
+//  question # 9
+
+1 & 2
+
+//  question # 10
+
+null
+
+//  question # 11
+
+1 & undefined
+
+//  question # 12
+
+3
+
+//  question # 13
+
+if (age < 14 || age > 90) {
+  }
+
+if (age >= 14 && age <= 90) {
+  }
+
+  //  question # 14
+
+  first & third
+
+  //  question # 15
+
+  if (browser === 'Edge') {
+    alert("You've got the Edge!");
+  } else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+    alert('Okay we support these browsers too');
+  } else {
+    alert('We hope that this page looks ok!');
+  }
+
+//   question # 16
+
+  let a = +prompt('a?', '');
+
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert('2,3');
+    break;
+  default:
+    break;
+}
